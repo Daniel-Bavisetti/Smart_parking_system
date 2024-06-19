76fbb04aaf383c07e4bdae38321f13a06037d316
Daniel-Bavisetti <93106492+Daniel-Bavisetti@users.noreply.github.com>
2024-06-19 10:24:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+from ultralytics import YOLO
+import cv2
+import numpy as np
+from sort.sort import Sort
+from util import get_car, read_license_plate, write_csv
+
+# Initialize variables
+results = {}
+mot_tracker = Sort()
+
+# Load models
+coco_model = YOLO('yolov8n.pt')
+license_plate_detector = YOLO('C:/Users/Daniel/Desktop/Coding/VRITIKA_Internship/number_plate_detection/models/license_plate_detector.pt')
+
+# Load video
+cap = cv2.VideoCapture('./parklowcut.mp4')
+vehicles = [2, 3, 5, 7]
+
+# Read frames
+frame_nmr = -1
+ret = True
+while ret:
+    frame_nmr += 1
+    ret, frame = cap.read()
+    if ret:
+        results[frame_nmr] = {}
+        
+        # Detect vehicles
+        detections = coco_model(frame)[0]
+        detections_ = []
+        for detection in detections.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = detection
+            if int(class_id) in vehicles:
+                detections_.append([x1, y1, x2, y2, score])
+        
+        # Convert detections_ to a numpy array and ensure it has the right shape
+        detections_ = np.asarray(detections_)
+        if len(detections_) > 0:
+            print("Detections array shape:", detections_.shape)
+        else:
+            print("No vehicle detections in this frame.")
+
+        # Track vehicles
+        track_ids = mot_tracker.update(detections_)
+
+        # Detect license plates
+        license_plates = license_plate_detector(frame)[0]
+        for license_plate in license_plates.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = license_plate
+
+
+            # Assign license plate to car
+            xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, track_ids)
+            if car_id != -1:
+                # Crop license plate
+                license_plate_crop = frame[int(y1):int(y2), int(x1):int(x2), :]
+                # Process license plate
+                license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
+                _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
+                # Read license plate number
+                license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
+                if license_plate_text is not None:
+                    results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+                                                  'license_plate': {'bbox': [x1, y1, x2, y2],
+                                                                    'text': license_plate_text,
+                                                                     'bbox_score': score,
+                                                                    'text_score': license_plate_text_score}}
+
+# Write results to csv
+write_csv(results, './test.csv')
